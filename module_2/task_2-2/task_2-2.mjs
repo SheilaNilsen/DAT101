a1b54304ffd1d5f50075c7b61209b62b95c8a69f
SheilaNilsen <babesheila123@icloud.com>
2024-09-28 17:28:53 +0200

    Update task_2-2.mjs

--- module_2/task_2-2/task_2-2.mjs
+++ module_2/task_2-2/task_2-2.mjs
@@ -3,9 +3,8 @@ import { initPrintOut, printOut, newLine } from "../../common/script/utils.mjs";
 initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Oppgave 1 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
 <p id= "calculation"></p>
-  
+/* Put your code below here!*/  
 <script>
 let a = 2;
 let b = 4;
@@ -16,17 +15,19 @@ x = -34
 
 document.getElementById("calculation").innerHTML = x;  
   </script>
-  
-  </body>
+   
   </html>
 <div class = "text">
 printOut(" original expression, 2 + 3 * 2 - 4 * 6 = -34 ");
 <div class = "text">
-printOut(" modified expression, 2 + (3 * (2 - 4) ) * 6 = -34 and in javascript expressions shows as a + (c * (a - b) ) * d = -34);
+printOut(" modified expression, 2 + (3 * (2 - 4) ) * 6 = -34 ");
+<div class = "text">
+printOut(" in javascript expressions shows as a + (c * (a - b) ) * d = -34");
 <div class = "text">
 printOut(" by using the declared variables in calculation we got the same result of the expressions as -34.");
 
-printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
+</body>
+printOut("--- Oppgave 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
 printOut("Replace this with you answer!");
 printOut(newLine);
